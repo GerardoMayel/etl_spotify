34963cc1dc6f2ac902e5ee1551f4ad14e81e6d55
Gerardo Mayel Fernández Alamilla <geramfernandez@gmail.com>
2021-08-03 18:44:07 -0500

    adding aload code

---
+++ etl_spotify.py
@@ -0,0 +1,28 @@
+
+
+import sqlalchemy
+import pandas as pd
+from sqlalchemy.orm import sessionmaker
+import requests
+import json
+from datetime import datetime
+import datetime
+import sqlite3
+
+DATEBASE_LOCATION = "sqlite:///my_played_tracks.sqlite"
+
+USER_ID = ""
+TOKEN = ""
+
+
+
+
+def run():
+    pass
+
+
+
+if __name__ == "__main__":
+    run()
+
+
